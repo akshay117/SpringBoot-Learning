04634201fd8377cd54d3e17d4898eceb4b80077e
akshay117 <akshayvanil117@gmail.com>
2021-09-22 23:51:14 +0530

    React to Db done

--- FullStack/React/react-client/src/component/Project/AddProject.js
+++ FullStack/React/react-client/src/component/Project/AddProject.js
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import { ReactPropTypes } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { createProject } from "../../actions/projectActions";
 
 class AddProject extends Component {
   constructor() {
@@ -29,7 +33,7 @@ class AddProject extends Component {
       endDate: this.state.endDate,
     };
 
-    console.log(newProject);
+    this.props.createProject(newProject, this.props.history);
   }
 
   render() {
@@ -117,4 +121,8 @@ class AddProject extends Component {
   }
 }
 
-export default AddProject;
+AddProject.propTypes = {
+  createProject: PropTypes.func.isRequired,
+};
+
+export default connect(null, { createProject })(AddProject);
