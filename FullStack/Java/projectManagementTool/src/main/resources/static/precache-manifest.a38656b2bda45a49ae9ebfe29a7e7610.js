60c9485e9d2ecf69d23f935057c9cc2af36f0694
akshay117 <akshayvanil117@gmail.com>
2021-10-01 11:19:38 +0530

    Added React Build

---
+++ FullStack/Java/projectManagementTool/src/main/resources/static/precache-manifest.a38656b2bda45a49ae9ebfe29a7e7610.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3f2e957683a60fa28a43c930e9065fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39ee9d94a6e5792d8c0d",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "6013c4585ec41de9d71f",
+    "url": "/static/css/main.d3c1742d.chunk.css"
+  },
+  {
+    "revision": "39ee9d94a6e5792d8c0d",
+    "url": "/static/js/2.46708143.chunk.js"
+  },
+  {
+    "revision": "6013c4585ec41de9d71f",
+    "url": "/static/js/main.ea691a48.chunk.js"
+  },
+  {
+    "revision": "a58a2d5e2e2bf522cd66",
+    "url": "/static/js/runtime-main.c2ff858f.js"
+  }
+]);
